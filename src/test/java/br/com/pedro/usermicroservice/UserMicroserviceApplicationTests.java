95db75e22fffb773597c1b5a04320b52dbad06ab
pedroap94 <pedroalmodovar94@gmail.com>
2022-02-07 14:01:05 -0300

    first commit

---
+++ src/test/java/br/com/pedro/usermicroservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.usermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
